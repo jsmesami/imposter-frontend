1a2b6ae5151665126951d9b17fe615bee9d29b1c
Ondra Nejedlý <jsmesami@gmail.com>
2017-09-03 16:02:44 +0200

    Add some basic components

---
+++ src/imposter/components/basic.cljs
@@ -0,0 +1,51 @@
+(ns imposter.components.basic
+  (:require
+    [clojure.string :refer [join]]
+    [imposter.utils.bem :refer [bem] :as bem]))
+
+
+(defn svg
+  [id]
+  (let [icon-name (name id)]
+    [:svg {:role "img"}
+     [:use {:xlink-href (str "/assets/icons/" icon-name ".svg#" icon-name)}]]))
+
+
+(defn icon
+  [id & [modifiers]]
+  [:span
+   {:class (bem/bm :icon (into [id] modifiers))}
+   [svg id]])
+
+
+(defn button
+  [label & {:keys [enabled? busy? on-click attrs modifiers icon-name]
+            :or {enabled? true
+                 busy? false
+                 attrs {}
+                 modifiers []}}]
+  (let [states [(when-not enabled? :disabled) (when busy? :busy)]
+        default-attrs {:class (bem/bm "button" (into states modifiers))
+                       :on-click #(when (and (not busy?) enabled? on-click)
+                                    (on-click %))}]
+    [:button
+     (merge default-attrs attrs)
+     label
+
+     (if busy?
+       [icon "spinner" ["spinning"]]
+       (when icon-name [icon icon-name]))]))
+
+
+(defn input
+  [& {:keys [enabled? value on-change attrs modifiers]
+      :or {enabled? true
+           attrs {}
+           modifiers []}}]
+  (let [default-attrs {:default-value value
+                       :class (bem :form-group :input modifiers)
+                       :on-change #(when (and on-change enabled?)
+                                     (on-change (-> % .-target .-value)))
+                       :type :text}]
+    [:div
+     [:input (merge default-attrs attrs)]]))
---
+++ src/imposter/components/core.cljs
@@ -0,0 +1,1 @@
+(ns imposter.components.core)
