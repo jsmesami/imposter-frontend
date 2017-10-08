417e6c0d57b4f4b95445547f83b84724457653fa
Ondra Nejedlý <jsmesami@gmail.com>
2017-10-08 17:18:32 +0200

    Add navbar

---
+++ src/imposter/components/navbar.cljs
@@ -0,0 +1,16 @@
+(ns imposter.components.navbar
+  (:require
+    [imposter.components.basic :refer [icon]]
+    [imposter.utils.bem :as bem]))
+
+
+(def module-name "navbar")
+
+
+(defn navbar
+  [& children]
+  [:div {:class module-name}
+   (into [:div {:class bem/be module-name "children"}] children)
+   [:div {:class bem/be module-name "logo"}
+    [:img {:src "/assets/images/logo_MLP"
+           :alt "Městská knihovna v Praze"}]]])
